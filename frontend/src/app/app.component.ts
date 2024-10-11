564443a816e666d476cb82a09b02d0c20744dfee
PK-170 <pawan59c@gmail.com>
2024-10-11 18:44:30 +1100

    fixed some files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,5 +9,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title:string = '';
+  name:string;
+  age:number = 23;
+
+constructor() {
+  this.title = 'first constructor';
+  this.name = "second row"
+}
+
+
 }
