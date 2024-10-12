131808491344a1bea352c7724b660b202b3a0ca0
PK-170 <pawan59c@gmail.com>
2024-10-13 10:53:16 +1100

    fixed some files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+
+
+interface User{
+  name:string;
+  firstName:string;
+  lastName:string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,6 +18,24 @@ import { HomeComponent } from './home/home.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  data:User[] = [
+    {
+      name: 'Ming',
+      firstName:'Mingh',
+      lastName:'Young'
+    },
+    {
+      name: 'Sai',
+      firstName:'Sai',
+      lastName:'Panda'
+    },
+    {
+      name: 'Zihan',
+      firstName:'Zihan',
+      lastName:'Young'
+    }
+  ]
   
 
   
