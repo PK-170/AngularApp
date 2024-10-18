96c18d55e4bb924d6e2ee64b28b78ef222ec5632
PK-170 <pawan59c@gmail.com>
2024-10-18 20:51:42 +1100

    added template form comp

---
+++ frontend/src/app/user-template-form/user-template-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-template-form',
+  templateUrl: './user-template-form.component.html',
+  styleUrls: ['./user-template-form.component.css']
+})
+export class UserTemplateFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
