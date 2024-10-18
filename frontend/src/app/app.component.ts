7254616493b43efbcb68104c1e9c57c8cc716892
PK-170 <pawan59c@gmail.com>
2024-10-18 21:08:45 +1100

    added template form comp

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { Router, RouterOutlet } from '@angular/router';
 import { CommonModule, NgIf } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { StockBaseModule } from './stock-base/stock-base.module';
+import { LoginComponent } from "./login/login.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule, StockBaseModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, StockBaseModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
